ba424c7264f7e326f0bddae65be2c5eac5e9c3b1
ebagabee <alvesantos.gh@gmail.com>
2026-05-23 15:09:52 -0300

    solve: linked_list_queue

--- ch9_linked_lists/CH9_L7_linked_list_queue/main.py
+++ ch9_linked_lists/CH9_L7_linked_list_queue/main.py
@@ -3,20 +3,27 @@ from node import Node
 
 class LinkedList:
     def add_to_head(self, node):
+        if self.head is None:
+            self.tail = node
+            
         node.set_next(self.head)
         self.head = node
 
     def add_to_tail(self, node):
         if self.head is None:
             self.head = node
+            self.tail = node
             return
-        last_node = self.head
-        for current_node in self:
-            last_node = current_node
-        last_node.set_next(node)
+
+        if self.tail is not None:
+            last_node = self.tail
+            last_node.set_next(node)
+
+            self.tail = node
 
     def __init__(self):
         self.head = None
+        self.tail = None
 
     # don't touch below this line
 
